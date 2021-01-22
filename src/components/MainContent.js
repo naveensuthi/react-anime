2b4173e56e21434dfc24b57fdfceec8532a184fa
naveensuthi <naveensuthi@gmail.com>
2021-01-22 10:55:59 +0530

    first react anime app

---
+++ src/components/MainContent.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import AnimeCard from "./AnimeCard";
+
+function MainContent(props) {
+    return (
+        <main>
+            <div className="main-head">
+                <form
+                    className="search-box"
+                    onSubmit={props.handleSearch}
+                >
+                    <input
+                        type="search"
+                        placeholder="Search for Anime ..."
+                        required
+                        value={props.search}
+                        onChange={e => props.setSearch(e.target.value)}
+                    />
+                </form>
+            </div>
+            <div className="anime-list">
+                {props.animeList.map(anime => (
+                    <AnimeCard
+                        anime={anime}
+                        key ={anime.mal_id}
+                    />
+                ))}
+            </div>
+        </main>
+    )
+}
+
+export default MainContent
